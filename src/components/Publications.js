59f082c13b5abb720dd695e4d59adf0b04b0f427
岩井望 <iy.nozomy@gmail.com>
2025-03-09 17:54:03 +0900

    sidebar-openのアイコンのリンク修正

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -10,7 +10,7 @@ function Publications() {
         <p>1. <span className="underline-name">Nozomu Iwai</span>, Title(undisclosed), Extended Abstract on CHI 2025 Late Breaking Work <a href="https://chi2025.acm.org/"><span className="url-text">[URL]</span></a></p>
       </div>
       <div className="publication-category">
-        <h3>Refereed Domestic Conference (Presentation)</h3>
+        <h3>Refereed Domestic Conference</h3>
         <p>1. <span className="underline-name">岩井 望</span>, 崔 明根, 坂本 大介, 小野哲夫. AR入力デバイスとしての身体装着型トラックボールの検討. インタラクション2024 情報処理学会.
         </p>
       </div>
