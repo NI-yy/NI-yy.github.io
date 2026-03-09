9cd7a0a3617cf56073fc0f74e4e12063986890f2
岩井望 <iy.nozomy@gmail.com>
2026-03-09 10:37:17 +0900

    chore: フッターの著作権年を2026年に更新

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>@ 2025 Nozomu Iwai</p>
+      <p>@ 2026 Nozomu Iwai</p>
       <p>This page was inspired by <a href="https://github.com/manuelernestog/manuelernestog.github.io"><span className="url-text">manuelerrigitto.github.io</span></a> and the personal pages of some of my lab mates.</p>
     </footer>
   );
